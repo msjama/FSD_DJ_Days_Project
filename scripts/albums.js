13c3add49b0f1c94d56d8b4ca79deb0b63582ec6
msjama <msjama@gmail.com>
2023-03-07 19:25:48 -0600

    adding DJ days project

--- scripts/albums.js
+++ scripts/albums.js
@@ -6,3 +6,37 @@
   Optionally, you can also add an "artwork" property with an image of the album cover.
 */
 
+/* Iconic albums of all time*/
+
+const album = [
+  {
+    name: 'Get Rich or Die Trying',
+    artist: '50 cent',
+    genre: 'Hip-hop',
+    artwork: 'Images/Get_Rich_Or_Die_Tryin'.jpeg'
+  },
+  { 
+    name: 'Renaissance',
+    artist: 'Beyonze',
+    genre: 'R&B',
+    artwork: 'Images/Reeves_Beyonce_2.png'
+  },
+  { 
+    name: 'Love, Damini',
+    artist: 'Burna Boy',
+    genre: 'Afro beats',
+    artwork: 'Images/Burna_Boy_-_Love,_Damini.png'
+  },
+  { 
+    name: 'Rave and Roses',
+    artist: 'Rema',
+    genre: 'Afro beats',
+    artwork: 'Images/rema_rave_&_roses-portada.jpeg'
+  },
+  { 
+    name: 'Old Romance',
+    artist: 'Tekno',
+    genre: 'Afro beats',
+    artwork: 'Images/Tekno-Old-Romance-Album-Artwork.jpeg'
+  }
+];
